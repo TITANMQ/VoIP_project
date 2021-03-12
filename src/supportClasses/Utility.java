ffaf83b8d3ff5fe3bcff69db628c60ff18bc8d7b
Elijah Rose <elijahr0001@gmail.com>
2021-03-12 15:18:50 +0000

    Add Utility class with blockInterleave method

---
+++ src/supportClasses/Utility.java
@@ -0,0 +1,43 @@
+package supportClasses;
+
+import java.util.Arrays;
+
+public class Utility {
+
+
+    public static Object[] blockInterleave(Object [] b){
+        Object[] result = new Object[b.length];
+        int d = 4; //TODO change this from hardcoded
+        int c = 0;
+
+        for(int i = 0; i < d; i++) {
+
+            for (int j = 0; j < d; j++) {
+                Object x = b[c];
+                int p = j * d + (d - 1 - i);
+                result[p] = x;
+                c++;
+            }
+
+        }
+        return result;
+    }
+
+
+    //test harness
+    public static void main(String[] args) {
+        Object[] y = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+//        System.out.println(Arrays.toString(blockInterleave(x)));
+
+        y = blockInterleave(y);
+        System.out.println(Arrays.toString(y));
+        y = blockInterleave(y);
+        System.out.println(Arrays.toString(y));
+        y = blockInterleave(y);
+        System.out.println(Arrays.toString(y));
+        y = blockInterleave(y);
+        System.out.println(Arrays.toString(y));
+
+    }
+}
